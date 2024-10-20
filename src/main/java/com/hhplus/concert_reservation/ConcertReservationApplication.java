eb364569d12eeb7e3ffaa2c2fd0becacdb842b12
jhshin29 <shinji03624@gmail.com>
2024-10-20 23:39:25 +0900

    chore: change folder name from example to hhplus

--- src/main/java/com/hhplus/concert_reservation/ConcertReservationApplication.java
+++ src/main/java/com/hhplus/concert_reservation/ConcertReservationApplication.java
@@ -1,4 +1,4 @@
-package com.example.concert_reservation;
+package com.hhplus.concert_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hhplus/concert_reservation/ConcertReservationApplicationTests.java
+++ src/test/java/com/hhplus/concert_reservation/ConcertReservationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.concert_reservation;
+package com.hhplus.concert_reservation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
